fb053eae3b67c28a608d2633dd9f7edfaa005d91
WU Yang <1015497577@qq.com>
2020-03-20 18:16:34 +0800

    Add LDA running code.

--- data-raw/scripts.for.SBS1SBS5/Run.MultiModalMuSig.jl
+++ data-raw/scripts.for.SBS1SBS5/Run.MultiModalMuSig.jl
@@ -22,28 +22,87 @@ seedsInUse = [1, 691, 1999, 3511, 8009,
                 27847, 34637, 49081, 75679, 103333,
                 145879, 200437, 310111, 528401, 1076753];
 
-## Example from README
-if 0
-    snv_counts = CSV.read("data/brca-eu_snv_counts.tsv", delim='\t')
-    sv_counts = CSV.read("data/brca-eu_sv_counts.tsv", delim='\t')
 
-    X = format_counts_mmctm(snv_counts, sv_counts)
-    model = MMCTM([7, 7], [0.1, 0.1], X)
-    fit!(model, tol=1e-5)
 
-    snv_signatures = DataFrame(hcat(model.ϕ[1]...))
-    sv_signatures = DataFrame(hcat(model.ϕ[2]...))
+## Cycle for each dataset to run MultiModalMuSig.LDA
+for seedInUse in seedsInUse
+	for datasetName in datasetNames
 
-    snv_signatures[:term] = snv_counts[:term]
-    snv_signatures = melt(
-        snv_signatures, :term, variable_name=:signature, value_name=:probability
-    )
-    snv_signatures |> @vlplot(
-        :bar, x={:term, sort=:null}, y=:probability, row=:signature,
-        resolve={scale={y=:independent}}
+		snv_counts = CSV.read(datasetName*"/sp.sp/ExtrAttr/MultiModalMuSig.LDA.results/seed."*
+		string(seedInUse)*"/ground.truth.syn.catalog.tsv", delim='\t');
+
+		## Run model fitting
+		## After the model is fitted, we may obtain extracted signatures and attributed exposures
+		X = format_counts_lda(snv_counts);
+
+		## Save the likelihood for each K.
+		likelihoods = [];
+		for K in 1:10
+			## Specify seed used before fitting model for each dataset,
+			## for each signature number (K)
+			Random.seed!(seedInUse);
+			model = LDA(K, 0.1, 0.1, 96, X);
+			fit!(model, tol=1e-5);
+			likelihood = MultiModalMuSig.calculate_loglikelihood(model);
+			append!(likelihoods, likelihood);
+		end
+
+		## Find which K is the best likelihood.
+		max_likelihood = maximum(likelihoods);
+		KBest = [i for (i, x) in enumerate(likelihoods) if x == max_likelihood];
+		KBest = KBest[1];
+
+		## Obtain extracted signatures given the best K.
+		## In MultiModalMuSig format, later converted to ICAMS format
+		Random.seed!(seedInUse);
+		model = LDA(KBest, 0.1, 0.1, 96, X); ## You cannot write [KBest] here!
+		## [KBest] an nested array!
+		fit!(model, tol=1e-5);
+		snv_signatures = DataFrame(model.β);
+		sig_names = names(snv_signatures);
+		snv_signatures[:term] = snv_counts[:term];
+		snv_signatures = snv_signatures[:,[:term;sig_names]]; # Shuffle the columns of signature DataFrame
+
+		# You can't change row names by subsetting!
+		CSV.write(datasetName*"/sp.sp/ExtrAttr/MultiModalMuSig.LDA.results/seed."*
+		string(seedInUse)*"/extracted.signatures.tsv",
+		snv_signatures,
+		delim = "\t");
+
+		## Obtain attributed exposures
+		snv_exposures = DataFrame(model.θ);
+		sample_names = names(snv_counts)[2:(size(snv_counts)[2])];
+		names!(snv_exposures, sample_names);# Set column names for DataFrame snv_exposures
+		# For each tumor, Multiply exposure proportion with the total number of mutations
+		for sample_name in sample_names
+			snv_exposures[:,sample_name] = snv_exposures[:,sample_name] * sum(snv_counts[:,sample_name]);
+		end
+		# Add signature names.
+		# Julia DataFrame does not support row names to be "character"!
+		snv_exposures[:signature_name] = sig_names
+		snv_exposures = snv_exposures[:,[:signature_name; sample_names]]
+
+		# Write attributed exposures
+		CSV.write(datasetName*"/sp.sp/ExtrAttr/MultiModalMuSig.LDA.results/seed."*
+		string(seedInUse)*"/attributed.exposures.tsv",
+		snv_exposures,
+		delim = "\t")
+	end
 end
 
-## Cycle for each dataset
+
+
+
+
+
+
+
+
+
+
+
+
+## Cycle for each dataset to run MultiModalMuSig.CTM
 for seedInUse in seedsInUse
 	for datasetName in datasetNames
 
