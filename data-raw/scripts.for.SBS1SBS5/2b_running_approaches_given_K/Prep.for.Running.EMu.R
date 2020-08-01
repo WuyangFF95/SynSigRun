a122d5b1c029e857f37e2bfa122312786c91024c
WuyangFF95 <wuyangff95@gmail.com>
2020-08-01 16:51:40 +0800

    Add scripts for running more approaches given K=2.

---
+++ data-raw/scripts.for.SBS1SBS5/2b_running_approaches_given_K/Prep.for.Running.EMu.R
@@ -0,0 +1,31 @@
+
+# Set working directory to "<SynSigRun Home>/data-raw/scripts.for.SBS1SBS5"
+# before running this script.
+# SynSigRun home can be retrieved by usethis::proj_path
+#
+# PATH <- paste0(usethis::proj_path,"/data-raw/scripts.for.SBS1SBS5")
+# setwd(PATH)
+
+## Load required packages
+library(ICAMS)
+library(SynSigRun)
+
+## Specify slopes and Rsqs for the datasets
+slopes <- c(0.1,0.5,1,2,10)
+Rsqs <- c(0.1,0.2,0.3,0.6)
+datasetNames <- c()
+for(slope in slopes)
+  for(Rsq in Rsqs)
+    datasetNames <- c(datasetNames, paste0("S.",slope,".Rsq.",Rsq))
+
+
+for(datasetName in datasetNames){
+  for(nrun in 1:20){
+    out.dir <- paste0(datasetName,"/sp.sp/ExtrAttrExact/EMu.results/run.",nrun)
+    dir.create(out.dir, recursive = TRUE)
+    SynSigRun::CreateEMuOutput(
+      catalog = paste0(datasetName,"/sp.sp/ground.truth.syn.catalog.csv"),
+      out.dir = out.dir,
+      overwrite = F)
+  }
+}
---
+++ data-raw/scripts.for.SBS1SBS5/2b_running_approaches_given_K/Run.sigfit.EMu.R
@@ -0,0 +1,48 @@
+
+# Set working directory to "<SynSigRun Home>/data-raw/scripts.for.SBS1SBS5"
+# before running this script.
+# SynSigRun home can be retrieved by usethis::proj_path
+#
+# PATH <- paste0(usethis::proj_path,"/data-raw/scripts.for.SBS1SBS5")
+# setwd(PATH)
+
+## Load required packages
+library(ICAMS)
+library(SynSigRun)
+library(sigfit)
+library(rstan)
+library(rstantools)
+
+
+
+## Specify slopes and Rsqs for the datasets
+slopes <- c(0.1,0.5,1,2,10)
+Rsqs <- c(0.1,0.2,0.3,0.6)
+datasetNames <- c()
+for(slope in slopes)
+  for(Rsq in Rsqs)
+    datasetNames <- c(datasetNames, paste0("S.",slope,".Rsq.",Rsq))
+
+## Specify 20 seeds used in software running
+seedsInUse <- c(1, 691, 1999, 3511, 8009,
+                9902, 10163, 10509, 14476, 20897,
+                27847, 34637, 49081, 75679, 103333,
+                145879, 200437, 310111, 528401, 1076753)
+
+
+
+## Run approaches sigfit.NMF and sigfit.EMu
+for(seedInUse in seedsInUse){
+  for(datasetName in datasetNames){
+    Runsigfit(
+      input.catalog = paste0(datasetName, "/sp.sp/ground.truth.syn.catalog.csv"),
+      out.dir = paste0(datasetName,
+                       "/sp.sp/ExtrAttrExact/sigfit.EMu.results/seed.",
+                       seedInUse),
+      model = "emu",
+      CPU.cores = 10,
+      K.exact = 2,
+      overwrite = TRUE)
+  }
+}
+
