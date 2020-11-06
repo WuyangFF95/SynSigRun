ca4d243f9282a2f48eef647b9dae6d26448c2ecf
WuyangFF95 <wuyangff95@gmail.com>
2020-11-06 09:56:43 +0800

    Version 0.0.4.9004: Added scripts for running computaitonal approach sigminer.

---
+++ R/Runsigminer.R
@@ -0,0 +1,174 @@
+#' Install sigminer from Bioconductor,
+#' also installing its dependent package, NMF.
+#'
+#' @keywords internal
+Installsigminer <- function(){
+  message("Installing sigminer from Bioconductor...\n")
+  if (!requireNamespace("BiocManager", quietly = TRUE))
+    utils::install.packages("BiocManager")
+  BiocManager::install("sigminer")
+}
+
+
+
+#' Run sigminer extraction and attribution on a spectra catalog file
+#'
+#' @param input.catalog File containing input spectra catalog.
+#' Columns are samples (tumors), rows are mutation types.
+#'
+#' @param out.dir Directory that will be created for the output;
+#' abort if it already exits.  Log files will be in
+#' \code{paste0(out.dir, "/tmp")}.
+#'
+#'
+#' @param CPU.cores Number of CPUs to use in running
+#' sigminer. For a server, 30 cores would be a good
+#' choice; while for a PC, you may only choose 2-4 cores.
+#' By default (CPU.cores = NULL), the CPU.cores would be equal
+#' to \code{(parallel::detectCores())/2}, total number of CPUs
+#' divided by 2.
+#'
+#' @param seedNumber Specify the pseudo-random seed number
+#' used to run SomaticSignatures. Setting seed can make the
+#' attribution of SomaticSignatures repeatable.
+#' Default: 1.
+#'
+#' @param K.max \code{K.max} is the maximum number of signatures
+#' users expect to active in \code{input.catalog}.
+#' As this approach cannot specify \code{K.exact}, you can specify
+#' \code{K.max} = \code{K.exact} If you know exactly how many signatures
+#' are active in the \code{input.catalog}.
+#' On the other hand, you may specify \code{max(K.range)} if you don't
+#' know how many signatures are active in the \code{input.catalog}.
+#'
+#' @param test.only If TRUE, only analyze the first 10 columns
+#' read in from \code{input.catalog}.
+#' Default: FALSE
+#'
+#' @param overwrite If TRUE, overwrite existing output.
+#' Default: FALSE
+#'
+#' @return A list contains: \itemize{
+#' \item $signature extracted signatures,
+#' \item $exposure inferred exposures,
+#' } of \code{sigminer}, invisibly.
+#'
+#' @details Creates several
+#'  files in \code{out.dir}. These are:
+#'  TODO(Steve): list the files
+#'
+#'  TODO(Wuyang)
+#'
+#' @importFrom utils capture.output
+#'
+#' @export
+
+Runsigminer <-
+  function(input.catalog,
+           out.dir,
+           CPU.cores = NULL,
+           seedNumber = 1,
+           K.max = NULL,
+           test.only = FALSE,
+           overwrite = FALSE) {
+
+    ## Install sigminer, if not found in library
+    if ("sigminer" %in% rownames(utils::installed.packages()) == FALSE)
+      Installsigminer()
+
+
+    ## Set seed
+    set.seed(seedNumber)
+    seedInUse <- .Random.seed  ## Save the seed used so that we can restore the pseudorandom series
+    RNGInUse <- RNGkind() ## Save the random number generator (RNG) used
+
+
+    ## Read in spectra data from input.catalog file
+    ## spectra: spectra data.frame in ICAMS format
+    spectra <- ICAMS::ReadCatalog(input.catalog,
+                                  strict = FALSE)
+    if (test.only) spectra <- spectra[ , 1:10]
+    ## convSpectra: convert the ICAMS-formatted spectra catalog
+    ## into a matrix which sigminer accepts:
+    ## 1. Remove the catalog related attributes in convSpectra
+    ## 2. Transpose the catalog
+    convSpectra <- spectra
+    class(convSpectra) <- "matrix"
+    attr(convSpectra,"catalog.type") <- NULL
+    attr(convSpectra,"region") <- NULL
+    dimnames(convSpectra) <- dimnames(spectra)
+    sample.number <- dim(spectra)[2]
+    convSpectra <- t(convSpectra)
+
+    ## Create output directory
+    if (dir.exists(out.dir)) {
+      if (!overwrite) stop(out.dir, " already exits")
+    } else {
+      dir.create(out.dir, recursive = T)
+    }
+
+    ## CPU.cores specifies number of CPU cores to use.
+    ## If CPU.cores is not specified, CPU.cores will
+    ## be equal to the minimum of 30 or (total cores)/2
+    if(is.null(CPU.cores)){
+      CPU.cores = min(30,(parallel::detectCores())/2)
+    } else {
+      stopifnot(is.numeric(CPU.cores))
+    }
+
+
+    ## Before running NMF packge,
+    ## Load it explicitly to prevent errors.
+    requireNamespace("NMF")
+
+    ## Run sigminer using ICAMS-formatted spectra catalog
+    print(paste0("Assuming there are at most ",K.best," signatures active in input spectra."))
+    assess <- sigminer::sig_auto_extract(
+      nmf_matrix = convSpectra,
+      result_prefix = "BayesNMF",
+      destdir = paste0(out.dir,"/sigminer.data/"),
+      method = "L1W.L2H",
+      strategy = "stable",
+      K0 = K.max,
+      nrun = 20,
+      niter = 2e+05,
+      tol = 1e-07,
+      cores = CPU.cores,
+      optimize = TRUE,
+      skip = FALSE,
+      recover = FALSE)
+    gc()
+    gc()
+    gc()
+    ## normalized signature matrix
+    extractedSignatures <- assess$Signature.norm
+    colnames(extractedSignatures) <-
+      paste("sigminer",1:ncol(extractedSignatures),sep=".")
+    extractedSignatures <- ICAMS::as.catalog(extractedSignatures,
+                                             region = "unknown",
+                                             catalog.type = "counts.signature")
+    ## Output extracted signatures in ICAMS format
+    ICAMS::WriteCatalog(extractedSignatures,
+                        paste0(out.dir,"/extracted.signatures.csv"))
+
+
+    ## Derive exposure count attribution results.
+    ## normalized exposure matrix
+    exposureCounts <- assess$Exposure
+    rownames(exposureCounts) <-
+      paste("sigminer",1:nrow(exposureCounts),sep=".")
+    ## Write exposure counts in ICAMS and SynSig format.
+    SynSigGen::WriteExposure(exposureCounts,
+                             paste0(out.dir,"/inferred.exposures.csv"))
+
+
+    ## Save seeds and session information
+    ## for better reproducibility
+    capture.output(sessionInfo(), file = paste0(out.dir,"/sessionInfo.txt")) ## Save session info
+    write(x = seedInUse, file = paste0(out.dir,"/seedInUse.txt")) ## Save seed in use to a text file
+    write(x = RNGInUse, file = paste0(out.dir,"/RNGInUse.txt")) ## Save seed in use to a text file
+
+    ## Return a list of signatures and exposures
+    invisible(list("signature" = extractedSignatures,
+                   "exposure" = exposureCounts))
+  }
---
+++ data-raw/scripts.for.SBS1SBS5/2b_running_approaches_given_K/Run.sigminer.R
@@ -0,0 +1,46 @@
+# Set working directory to "<SynSigRun Home>/data-raw/scripts.for.SBS1SBS5"
+# before running this script.
+# SynSigRun home can be retrieved by usethis::proj_path
+#
+# PATH <- paste0(usethis::proj_path,"/data-raw/scripts.for.SBS1SBS5")
+# setwd(PATH)
+
+
+## Load required packages
+library(ICAMS)
+library(SynSigRun)
+library(NMF)
+
+
+
+## Specify slopes and Rsqs for the datasets
+slopes <- c(0.1,0.5,1,2,10)
+Rsqs <- c(0.1,0.2,0.3,0.6)
+datasetNames <- c()
+for(slope in slopes)
+  for(Rsq in Rsqs)
+    datasetNames <- c(datasetNames, paste0("S.",slope,".Rsq.",Rsq))
+
+## Specify 20 seeds used in software running
+seedsInUse <- c(1, 691, 1999, 3511, 8009,
+                9902, 10163, 10509, 14476, 20897,
+                27847, 34637, 49081, 75679, 103333,
+                145879, 200437, 310111, 528401, 1076753)
+
+
+
+## Run Extraction and attribution package sigminer
+for(seedInUse in seedsInUse){
+  for(datasetName in datasetNames){
+
+    cat("\n===========================================\n")
+    cat(paste0("Running sigminer on data set ",datasetName," using seed ",seedInUse,"...\n"))
+    cat("\n===========================================\n")
+
+    Runsigminer(input.catalog = paste0(datasetName, "/sp.sp/ground.truth.syn.catalog.csv"),
+      out.dir = paste0(datasetName, "/sp.sp/ExtrAttrExact/sigminer.results/seed.", seedInUse),
+      CPU.cores = 10,
+      K.max = 2,
+      overwrite = TRUE)
+  }
+}
