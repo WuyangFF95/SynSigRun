dbbcab7d51dccdd7d7bfdc49c3467efdcbe99327
WuyangFF95 <wuyangff95@gmail.com>
2020-11-12 14:40:50 +0800

    Version 0.0.4.9103:
    mutSignatures and SparseSignatures are no longer included in SBS1-SBS5 paper, thus we moved scripts to run mutSignatures and SparseSignatures to path
    
    ./data-raw/additional.scripts.on.SBS1SBS5

---
+++ data-raw/additional.scripts.on.SBS1SBS5.datasets/2a_running_approaches_without_knowing_K/Run.mutSignatures.R
@@ -0,0 +1,50 @@
+
+# Set working directory to "<SynSigRun Home>/data-raw/scripts.for.SBS1SBS5"
+# before running this script.
+# SynSigRun home can be retrieved by usethis::proj_path
+#
+# PATH <- paste0(usethis::proj_path,"/data-raw/scripts.for.SBS1SBS5")
+# setwd(PATH)
+
+## Load required packages
+library(ICAMS)
+library(SynSigRun)
+library(mutSignatures)
+
+
+
+## Specify slopes and Rsqs for the datasets
+slopes <- c(0.1,0.5,1,2,10)
+Rsqs <- c(0.1,0.2,0.3,0.6)
+datasetNames <- c()
+for(slope in slopes)
+  for(Rsq in Rsqs)
+    datasetNames <- c(datasetNames, paste0("S.",slope,".Rsq.",Rsq))
+
+## Specify 20 seeds used in software running
+seedsInUse <- c(1, 691, 1999, 3511, 8009,
+                9902, 10163, 10509, 14476, 20897,
+                27847, 34637, 49081, 75679, 103333,
+                145879, 200437, 310111, 528401, 1076753)
+
+
+
+## Run mutSignatures.
+for(seedInUse in seedsInUse){
+  for(datasetName in datasetNames){
+
+    cat("\n===========================================\n")
+    cat(paste0("Running mutSignatures on data set ",datasetName," using seed ",seedInUse,"...\n"))
+    cat("\n===========================================\n")
+
+    RunmutSignatures(
+      input.catalog = paste0(datasetName, "/sp.sp/ground.truth.syn.catalog.csv"),
+      out.dir = paste0(datasetName, "/sp.sp/ExtrAttr/mutSignatures.results/seed.", seedInUse),
+      CPU.cores = 10,
+      iterations = 1000,
+      seedNumber = seedInUse,
+      K.range = c(2,10),
+      overwrite = T)
+  }
+}
+
