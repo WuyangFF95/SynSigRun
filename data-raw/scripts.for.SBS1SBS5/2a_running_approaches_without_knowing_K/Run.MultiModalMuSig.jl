b36c0be31f20ee60e7123cd90bf0807db2b5861a
Wu,Yang <wuyangff95@yahoo.com>
2021-07-04 17:00:16 +0800

    Fixed syntax errors and type errors in Run.MultiModalMuSig.jl

--- data-raw/scripts.for.SBS1SBS5/2a_running_approaches_without_knowing_K/Run.MultiModalMuSig.jl
+++ data-raw/scripts.for.SBS1SBS5/2a_running_approaches_without_knowing_K/Run.MultiModalMuSig.jl
@@ -36,14 +36,18 @@ seedsInUse = [1, 691, 1999, 3511, 8009,
                 9902, 10163, 10509, 14476, 20897,
                 27847, 34637, 49081, 75679, 103333,
                 145879, 200437, 310111, 528401, 1076753];
-
+seedsInUse = seedsInUse[1:5];
 
 
 ## Cycle for each dataset to run MultiModalMuSig.LDA
 for seedInUse in seedsInUse
     for datasetName in datasetNames
-        outDir = paste0(topLevelFolder4Run,"/MultiModalMuSig.LDA.results/",datasetName,"/seed.",seedInUse)
+        outDir = string(topLevelFolder4Run,"/MultiModalMuSig.LDA.results/",datasetName,"/seed.",seedInUse)
         snv_counts = CSV.read(outDir*"/ground.truth.syn.catalog.tsv", delim='\t');
+        
+        print("\n\n==================================\n")
+        print(string("Running MultiModalMuSig.LDA in directory ",outDir,"\n"))
+        print("\n==================================\n\n")
 
         ## Run model fitting
         ## After the model is fitted, we may obtain extracted signatures and inferred exposures
@@ -52,6 +56,7 @@ for seedInUse in seedsInUse
         ## Save the likelihood for each K.
         likelihoods = NamedArray( repeat([NaN],9) , (string.([2:10;])) );
         for K in 2:10
+            print("\nCalculating likelihood for K = "*string(K)*"...\n")
             ## Specify seed used before fitting model for each dataset,
             ## for each signature number (K)
             Random.seed!(seedInUse);
@@ -71,7 +76,7 @@ for seedInUse in seedsInUse
             ## calculate 1st-derivative using forward difference operator
             ## with spacing equals to 1.
             deriv1[string(K)] = likelihoods[string(K+1)] - likelihoods[string(K)];
-          else if K == 10 
+          elseif K == 10 
             ## For the largest possible K,
             ## calculate 1st-derivative using backward difference operator.
             deriv1[string(K)] = likelihoods[string(K)] - likelihoods[string(K-1)];
@@ -89,7 +94,7 @@ for seedInUse in seedsInUse
             ## calculate 1st-derivative of the 1st-derivative using forward difference operator
             ## with spacing equals to 1.
             deriv2[string(K)] = deriv1[string(K+1)] - deriv1[string(K)];
-          else if K == 10 
+          elseif K == 10 
             ## For the largest possible K,
             ## calculate 1st-derivative of the 1st-derivative using backward difference operator.
             deriv2[string(K)] = deriv1[string(K)] - deriv1[string(K-1)];
@@ -100,24 +105,25 @@ for seedInUse in seedsInUse
 
         ## Choose the smallest K where 2nd-derivative of likelihood function
         ## changes sign in its neighborhood.
+        KBest = NaN; # Must declare outside of for loop
+        
         for K in 2:10
           ## If deriv2["2"] and deriv2["3"] have opposite sign
           ## set KBest = 2
           if K == 2
             if sign(deriv2[string(K)]) * sign(deriv2[string(K+1)]) == -1
-              KBest = K
+              KBest = K;
               break
             end
           ## If deriv2[string(K-1)] and deriv2[string(K+1)] have opposite sign
           ## set KBest = 2
-          else if K < 10
+          elseif K < 10
             if sign(deriv2[string(K-1)]) * sign(deriv2[string(K+1)]) == -1
-              KBest = K
+              KBest = K;
               break
             end
-          end
           else
-            KBest = K
+            KBest = K;
           end
         end
 
@@ -172,8 +178,12 @@ end
 ## Cycle for each dataset to run MultiModalMuSig.CTM
 for seedInUse in seedsInUse
     for datasetName in datasetNames
-        outDir = paste0(topLevelFolder4Run,"/MultiModalMuSig.CTM.results/",datasetName,"/seed.",seedInUse)
+        outDir = string(topLevelFolder4Run,"/MultiModalMuSig.CTM.results/",datasetName,"/seed.",seedInUse)
         snv_counts = CSV.read(outDir*"/ground.truth.syn.catalog.tsv", delim='\t');
+
+        print("\n\n==================================\n")
+        print(string("Running MultiModalMuSig.CTM in directory ",outDir,"\n"))
+        print("\n==================================\n\n")
 
         ## Run model fitting
         ## After the model is fitted, we may obtain extracted signatures and inferred exposures
@@ -182,14 +192,17 @@ for seedInUse in seedsInUse
         ## Save the likelihood for each K.
         likelihoods = NamedArray( repeat([NaN],9) , (string.([2:10;])) );
         for K in 2:10
+            print("\nCalculating likelihood for K = "*string(K)*"...\n")
             ## Specify seed used before fitting model for each dataset,
             ## for each signature number (K)
             Random.seed!(seedInUse);
             model = MMCTM([K], [0.1], X);
             fit!(model, tol=1e-5);
             likelihood = MultiModalMuSig.calculate_loglikelihoods(model);
-            likelihoods[string(K)] = likelihood;
+            likelihoods[string(K)] = likelihood[1];
         end
+        print("\n\nFinished Calculating likelihood\n\n")
+
 
         ## Elbow method: Find the inflection point of log-likelihood
         ## Calculate the first derivative of likelihoods
@@ -201,7 +214,7 @@ for seedInUse in seedsInUse
             ## calculate 1st-derivative using forward difference operator
             ## with spacing equals to 1.
             deriv1[string(K)] = likelihoods[string(K+1)] - likelihoods[string(K)];
-          else if K == 10 
+          elseif K == 10 
             ## For the largest possible K,
             ## calculate 1st-derivative using backward difference operator.
             deriv1[string(K)] = likelihoods[string(K)] - likelihoods[string(K-1)];
@@ -219,7 +232,7 @@ for seedInUse in seedsInUse
             ## calculate 1st-derivative of the 1st-derivative using forward difference operator
             ## with spacing equals to 1.
             deriv2[string(K)] = deriv1[string(K+1)] - deriv1[string(K)];
-          else if K == 10 
+          elseif K == 10 
             ## For the largest possible K,
             ## calculate 1st-derivative of the 1st-derivative using backward difference operator.
             deriv2[string(K)] = deriv1[string(K)] - deriv1[string(K-1)];
@@ -230,26 +243,29 @@ for seedInUse in seedsInUse
 
         ## Choose the smallest K where 2nd-derivative of likelihood function
         ## changes sign in its neighborhood.
+        KBest = NaN; # Must declare outside of for loop
+        
         for K in 2:10
           ## If deriv2["2"] and deriv2["3"] have opposite sign
           ## set KBest = 2
           if K == 2
             if sign(deriv2[string(K)]) * sign(deriv2[string(K+1)]) == -1
-              KBest = K
+              KBest = K;
               break
             end
           ## If deriv2[string(K-1)] and deriv2[string(K+1)] have opposite sign
           ## set KBest = 2
-          else if K < 10
+          elseif K < 10
             if sign(deriv2[string(K-1)]) * sign(deriv2[string(K+1)]) == -1
-              KBest = K
+              KBest = K;
               break
             end
-          end
           else
-            KBest = K
+            KBest = K;
           end
         end
+
+        KBest = [KBest];
 
 
         ## Obtain extracted signatures given the best K.
